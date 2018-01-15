bfe02623e60c4b5c3afaa7111bb3c4889612d01d
jhamee23 <jhamee623@gmail.com>
2018-01-15 17:21:02 +0800

    fifth

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-<script>
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
-</script>
+$(document).ready(function() {
+    $("#aboutme").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".second").offset().top
+        }, 800);
+    })
+    $("#education").click(function(){
+    	$('html, body').animate({
+    		scrollTop: $(".third").offset().top
+    	}, 800);
+    })
+    $("#none").click(function(){
+    	$('html, body').animate({
+    		scrollTop: $(".banner").offset().top
+    	}, 800);
+    })
+    $("#contact").click(function(){
+    	$('html, body').animate({
+    		scrollTop: $(".contact").offset().top
+    	}, 800);
+    });
+        $("#aboutme").hide();
+    $(".show_hide").on("click", function () {
+        var txt = $("#aboutme").is(':visible') ? 'Read More' : 'Read Less';
+        $(".show_hide").text(txt);
+        $(this).next('#aboutme').slideToggle(200);
+});
