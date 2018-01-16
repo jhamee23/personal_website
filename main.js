81e63c8b15be315ff40c1b560f8d456fe80567f3
jhamee23 <jhamee623@gmail.com>
2018-01-16 17:25:49 +0800

    sixth

--- main.js
+++ main.js
@@ -19,9 +19,26 @@ $(document).ready(function() {
     		scrollTop: $(".contact").offset().top
     	}, 800);
     });
-        $("#aboutme").hide();
-    $(".show_hide").on("click", function () {
-        var txt = $("#aboutme").is(':visible') ? 'Read More' : 'Read Less';
-        $(".show_hide").text(txt);
-        $(this).next('#aboutme').slideToggle(200);
+
+$(".aboutme , .skills , .me").click(function() {
+        var obj = $(this);
+        $('.content3').animate({
+            width: "50%"
+        }, 600);
+        $(".information").animate({
+            width: "49.8%"
+        }, 600, function() {
+            $(".aboutme_info").fadeOut();
+            $(".skills_info").fadeOut();
+            $(".me_info").fadeOut();
+            if ($(obj).hasClass('aboutme')) {
+                $(".aboutme_info").fadeIn(600);
+            } else if ($(obj).hasClass('skills')) {
+                $(".skills_info").fadeIn(600);
+            } else if ($(obj).hasClass('me')) {
+                $(".me_info").fadeIn(600);
+            }
+
+        });
+    });
 });
